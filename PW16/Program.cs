74c33cfb9cb15db2d6cd5ac252d42dcd4608a7ac
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-10-01 05:16:15 +0600

    В заданный путь записывается данные, после считывает файл побайтно и определяет сколько раз был использован символ

---
+++ PW16/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PW16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\repos\PracticalWork16\PW16\Text.txt";
+            //var path = Console.ReadLine();
+            var symbols = new Dictionary<string, int>();
+            File.CreateText(path).Close();
+            using (var stream = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                stream.WriteLine("Galymzhan");
+                stream.WriteLine("Oralbayev   ");
+                stream.WriteLine("19 y.o");
+            }
+
+            using (var stream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                var bytes = 0;
+                for (int i = 0; i < stream.Length; i++)
+                {
+                    bytes = stream.ReadByte();
+                    var symbol = ((char)bytes).ToString();
+                    if (symbol == "\n")
+                    {
+                        symbol = "Переход в новую строку";
+                    }
+                    else if(symbol == " ")
+                    {
+                        symbol = "Пробел";
+                    }
+                    else if (symbol == "\r")
+                    {
+                        symbol = "Возврат каретки в 0";
+                    }
+                    if (!symbols.ContainsKey(symbol))
+                    {
+                        symbols.Add(symbol, 0);
+
+                    }
+                    symbols[symbol]++;
+                }
+
+                foreach (var symbol in symbols)
+                {
+                    Console.WriteLine($"{symbol.Key} - {symbol.Value}");
+                }
+            }
+
+
+
+
+        }
+    }
+}
